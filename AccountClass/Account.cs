174e0ba96bd2a7d1089806fec39d7c2db187e54c
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-28 12:11:47 -0400

    initial commit

---
+++ AccountClass/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccountClass {
+
+    class Account {
+        public string AccountNumber { get; set; }
+        private decimal Balance { get; set; } = 0.0M; //the capital M at the end tells it to use it as a decimal not a double 
+        public string Description { get; set; }
+
+        public decimal GetBalance() {
+            return Balance;
+        }
+
+        private bool CheckAmountIsPositive(decimal amount) { //got rid off the duplicate code (from Deposit & Withdrawal) by creating a new method 
+            if (amount < 0) {
+                Console.WriteLine("Amount cannot be negative.");
+                return false;
+            }
+            return true;
+        }
+
+        public void Deposit(decimal Amount) {
+            var valid = CheckAmountIsPositive(Amount);
+            if (valid == true) {
+                Balance += Amount; //if the statement is true we are retriving and setting the value of Balance
+            }
+            }
+
+        public void Withdraw(decimal Amount) {
+            var valid = CheckAmountIsPositive(Amount);
+            if (valid == true) {
+                if (Amount > Balance) {
+                    Console.WriteLine("Insufficient finds.");
+                }
+                else {
+                    Balance -= Amount;
+                }
+            }
+
+        
+        }
+    }
+}
--- AccountClass/Program.cs
+++ AccountClass/Program.cs
@@ -3,7 +3,24 @@
 namespace AccountClass {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            //Build systrem for Bank with multiple types of accounts
+
+            var acct1 = new Account(); // acct1 is the variable name
+            acct1.AccountNumber = "12345";
+
+            acct1.Description = "Primary Checking";
+            acct1.Deposit(50000);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+            acct1.Withdraw(52000);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+            acct1.Deposit(5);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
+            var balance = acct1.GetBalance();
+            Console.WriteLine($"{balance}");
+            acct1.Withdraw(-1000000);
+            Console.WriteLine($"{acct1.AccountNumber} {acct1.Description} {acct1.GetBalance()}");
         }
+
     }
 }
